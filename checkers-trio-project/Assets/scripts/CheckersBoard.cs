e54f0f8dd2b0bfe478c575d9f7cd5bcb47df9dd7
roysez <sergiobaluh@gmail.com>
2017-10-03 18:38:15 +0300

    Can hit enemies

--- checkers-trio-project/Assets/scripts/CheckersBoard.cs
+++ checkers-trio-project/Assets/scripts/CheckersBoard.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Text.RegularExpressions;
 
-public class CheckersBoard : MonoBehaviour {
+public class CheckersBoard : MonoBehaviour
+{
 
 	public GameObject greenPiecePrefab;
 	public GameObject redPiecePrefab;
@@ -12,23 +13,27 @@ public class CheckersBoard : MonoBehaviour {
 
 
 	private Piece selectedPiece;
-	private Regex regex = new Regex(@"\w*Piece\b", RegexOptions.None);
+	private Regex regex = new Regex (@"\w*Piece\b", RegexOptions.None);
 
 
 	private int countOfCompletedActions;
 	private GameManager gameManager;
 
+
+
+
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 		countOfCompletedActions = 0;
 		gameManager = GetComponent<GameManager> ();
 		currentPlayer = gameManager.currentPlayer;
-
 	}
 
 
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 
 		if (CheckPlayerCompletedAction ())
 			countOfCompletedActions++;
@@ -41,46 +46,64 @@ public class CheckersBoard : MonoBehaviour {
 
 	}
 
-	private bool CheckPlayerCompletedAction(){
+	private bool CheckPlayerCompletedAction ()
+	{
 		bool actionCompleted = false;
 
-		if (Input.GetMouseButtonDown(0))
-		{
+		if (Input.GetMouseButtonDown (0)) {
 			//Debug.Log("Mouse is down");
 
-			RaycastHit hitInfo = new RaycastHit();
-			bool hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo);
-			if (hit) 
-			{
+			RaycastHit hitInfo = new RaycastHit ();
+			bool hit = Physics.Raycast (Camera.main.ScreenPointToRay (Input.mousePosition), out hitInfo);
+			if (hit) {
 				Debug.Log ("Hit " + hitInfo.transform.gameObject.name);
 				if (hitInfo.transform.gameObject.tag == "BlackCell") {
-					Debug.Log ("It's working!");
+					
+					Vector3 cellPositionToMove = hitInfo.transform.position;
 
-					if (selectedPiece != null) {
 
-						float distance = Vector3.Distance (selectedPiece.transform.position, hitInfo.transform.position);
+
+					if (selectedPiece != null) 
+					{
+
+						float distance = Vector3.Distance (selectedPiece.transform.position,cellPositionToMove);
 
 						Debug.Log ("Distance " + distance);
 
-						//if (distance > 1.5f && distance < 4.4f) {
 
-						if (distance < 2.4f) {
 
-							Vector3 positionOfCell = hitInfo.transform.position;
-							positionOfCell.y = 0.22f;	
-							positionOfCell.z = positionOfCell.z - 0.35f;
-							selectedPiece.transform.position = positionOfCell;
+						if (distance > 1.5f && distance < 4.4f && CheckIfFree (cellPositionToMove))
+						{
+							Vector3 centerOfVectors = new Vector3 ((cellPositionToMove.x + selectedPiece.transform.position.x)/2,
+								cellPositionToMove.y,
+								(cellPositionToMove.z + selectedPiece.transform.position.z)/2	);
 
-							actionCompleted = true;
-						} else {
+								
+							if (distance < 2.4f) {
+								
+								MovePiece (cellPositionToMove);
+
+								actionCompleted = true;
+							} else if(!CheckIfEnemy(centerOfVectors)){
+								
+								Debug.Log ("МОЖНА БИТИ");
+								Hit (centerOfVectors);
+
+								MovePiece (cellPositionToMove);
+
+								actionCompleted = true;
+							
+							} else
+								countOfCompletedActions = 0;
+
+						} else
 							countOfCompletedActions = 0;
-						}
-
-						//}
-
+						
 						DeselectPiece ();
 					}
-				} else if (regex.IsMatch(hitInfo.transform.gameObject.tag)) {
+				} 
+				else if (regex.IsMatch (hitInfo.transform.gameObject.tag)) 
+				{
 
 
 					if (selectedPiece != null) {
@@ -89,36 +112,81 @@ public class CheckersBoard : MonoBehaviour {
 
 					} else if (GetPermission (hitInfo.transform.gameObject.tag)) {
 						selectedPiece = hitInfo.transform.gameObject.GetComponent<Piece> ();
-						selectedPiece.SetLight (true,selectedPiece.transform.position);
+						selectedPiece.SetLight (true, selectedPiece.transform.position);
 
 						actionCompleted = true;
-					} else 
+					} else
 						DeselectPiece ();
 
-				}else {
+				} else {
 					countOfCompletedActions = 0;
 					Debug.Log ("Nog");
 					DeselectPiece ();
 				}
 
 			} else {
-				Debug.Log("No hit");
+				Debug.Log ("No hit");
 			}
 			//Debug.Log("Mouse is down");
 		} 
 		return actionCompleted;
 	}
 
-	private bool GetPermission(string tag){
+	private bool GetPermission (string tag)
+	{
 		return tag.Equals (currentPlayer.colorOfPieces + "Piece");
 	}
 
-	private void DeselectPiece(){
+	private void DeselectPiece ()
+	{
 		if (selectedPiece != null) {
-			selectedPiece.SetLight (false,new Vector3());
+			selectedPiece.SetLight (false, new Vector3 ());
 			selectedPiece = null;
 		}
 	}
 
 
+	private bool CheckIfFree (Vector3 centre)
+	{
+		Vector3 pieceCenter = new Vector3 (centre.x, centre.y + 0.22f, centre.z - 0.35f);
+		Collider[] colliders = Physics.OverlapSphere (pieceCenter, 0.05f);
+		Debug.Log ("Size: " + colliders.Length);
+
+
+		if (colliders.Length == 0) {
+			return true;
+		} else
+			return false;
+	}
+
+	private void Hit(Vector3 center){
+		Vector3 pieceCenter = new Vector3 (center.x, center.y + 0.22f, center.z - 0.35f);
+		Collider[] colliders = Physics.OverlapSphere (pieceCenter, 0.05f);
+
+		Debug.Log ("Видалення");
+
+		colliders [0].gameObject.SetActive (false);
+
+	}
+
+	private void MovePiece(Vector3 cellPositionToMove){
+		cellPositionToMove.y = 0.22f;	
+		cellPositionToMove.z = cellPositionToMove.z - 0.35f;
+		selectedPiece.transform.position = cellPositionToMove;
+
+	}
+
+	private bool CheckIfEnemy(Vector3 center)
+	{
+		if(CheckIfFree(center)){
+			return false;
+		}
+			else {
+			return Physics.OverlapSphere (new Vector3 (center.x, center.y + 0.22f, center.z - 0.35f), 0.05f)[0]
+			.gameObject
+			.transform
+			.tag==currentPlayer.colorOfPieces+"Piece";
+			}
+
+	}
 }
